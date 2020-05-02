88c1aa96fe4d8b995afe920932ccbdf683c8f87f
Michael Phelps <michael.phelps@cpcc.edu>
2020-05-02 16:39:22 -0400

    Update branding

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Central Piedmont',
+      title: 'Central Piedmont Chatbot',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
